00c4f460587fb1145b604dc23978d4d907d0db0c
irum-shah78 <irams4812@gmail.com>
2024-08-29 23:22:55 +0500

    profile image

--- src/components/settingform/useSettingForm.ts
+++ src/components/settingform/useSettingForm.ts
@@ -1,113 +1,8 @@
-// import { useState, useEffect } from 'react';
-// import { useSession } from 'next-auth/react';
-// import { useRouter } from 'next/navigation';
-// import axios from 'axios';
-// import toast from 'react-hot-toast';
-
-// export const useProfileSettings = () => {
-//   const { data: session, status } = useSession();
-//   const router = useRouter();
-
-//   const [name, setName] = useState<string>('');
-//   const [email, setEmail] = useState<string>('');
-//   const [image, setImage] = useState<string>('');
-//   const [imageFile, setImageFile] = useState<File | null>(null);
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     if (session?.user) {
-//       setName(session.user.name || '');
-//       setEmail(session.user.email || '');
-//       setImage(session.user.image || '');
-//     }
-//   }, [session]);
-
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     setLoading(true);
-
-//     try {
-//       let imagePath = image;
-//       if (imageFile) {
-//         const reader = new FileReader();
-//         reader.readAsDataURL(imageFile);
-//         reader.onloadend = async () => {
-//           imagePath = reader.result as string; 
-
-//           const updateResponse = await axios.put('/api/update', {
-//             name,
-//             email,
-//             imagePath, 
-//           });
-
-//           if (updateResponse.status === 200) {
-//             toast.success('Profile updated successfully');
-//             router.refresh();
-//           } else {
-//             throw new Error('Profile update failed');
-//           }
-
-//           setLoading(false);
-//         };
-//       } else {
-//         const updateResponse = await axios.put('/api/update', {
-//           name,
-//           email,
-//         });
-
-//         if (updateResponse.status === 200) {
-//           toast.success('Profile updated successfully');
-//           router.refresh();
-//         } else {
-//           throw new Error('Profile update failed');
-//         }
-
-//         setLoading(false);
-//       }
-//     } catch (error: any) {
-//       toast.error(`Failed to update profile: ${error.message}`);
-//       setLoading(false);
-//     }
-//   };
-
-//   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const file = e.target.files?.[0];
-//     if (file) {
-//       setImageFile(file);
-//       const reader = new FileReader();
-//       reader.onloadend = () => {
-//         setImage(reader.result as string);
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   const handleChangePassword = () => {
-//     router.push('/changepassword');
-//   };
-
-//   return {
-//     name,
-//     setName,
-//     email,
-//     setEmail,
-//     image,
-//     setImage,
-//     loading,
-//     status,
-//     handleSubmit,
-//     handleImageChange,
-//     handleChangePassword,
-//   };
-// };
-
-
 import { useState, useEffect } from 'react';
-import { useSession, signOut } from 'next-auth/react';
+import { useSession} from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
 
 export const useProfileSettings = () => {
   const { data: session, status, update } = useSession();
@@ -127,52 +22,44 @@ export const useProfileSettings = () => {
     }
   }, [session]);
 
+  const updateProfile = async (name: string, email: string, imagePath: string) => {
+    try {
+      const updateResponse = await axios.put('/api/update', {
+        name,
+        email,
+        imagePath,
+      });
+
+      if (updateResponse.status === 200) {
+        toast.success('Profile updated successfully');
+        await update({ user: { ...session?.user, name, email, image: updateResponse.data.user.image } });
+
+        router.refresh();
+      } else {
+        throw new Error('Profile update failed');
+      }
+    } catch (error) {
+      toast.error('Error updating profile');
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
-  
+
     try {
       let imagePath = image;
+
       if (imageFile) {
         const reader = new FileReader();
         reader.readAsDataURL(imageFile);
         reader.onloadend = async () => {
-          imagePath = reader.result as string; 
-  
-          const updateResponse = await axios.put('/api/update', {
-            name,
-            email,
-            imagePath, 
-          });
-  
-          if (updateResponse.status === 200) {
-            // Refresh the session to get the updated user data
-            await signIn('credentials', { redirect: false });
-  
-            toast.success('Profile updated successfully');
-            router.refresh();
-          } else {
-            throw new Error('Profile update failed');
-          }
-  
+          imagePath = reader.result as string;
+          await updateProfile(name, email, imagePath); 
           setLoading(false);
         };
       } else {
-        const updateResponse = await axios.put('/api/update', {
-          name,
-          email,
-        });
-  
-        if (updateResponse.status === 200) {
-          // Refresh the session to get the updated user data
-          await signIn('credentials', { redirect: false });
-  
-          toast.success('Profile updated successfully');
-          router.refresh();
-        } else {
-          throw new Error('Profile update failed');
-        }
-  
+        await updateProfile(name, email, imagePath); 
         setLoading(false);
       }
     } catch (error: any) {
--- src/libs/AuthOptions.ts
+++ src/libs/AuthOptions.ts
@@ -1,85 +1,3 @@
-// import { AuthOptions } from 'next-auth';
-// import CredentialsProvider from 'next-auth/providers/credentials';
-// import GoogleProvider from 'next-auth/providers/google';
-// import prisma from '../libs/prismadb';
-// import bcrypt from 'bcrypt';
-
-// export const authOptions: AuthOptions = {
-//   providers: [
-//     CredentialsProvider({
-//       name: 'Credentials',
-//       credentials: {
-//         email: { label: 'Email', type: 'email' },
-//         password: { label: 'Password', type: 'password' },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials?.password) {
-//           throw new Error('Missing Credentials!');
-//         }
-
-//         try {
-//           const user = await prisma.user.findUnique({
-//             where: { email: credentials.email },
-//           });
-
-//           if (!user || !user.password) {
-//             throw new Error('Invalid Credentials!');
-//           }
-
-//           const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
-//           if (!isPasswordValid) {
-//             throw new Error('Invalid Credentials!');
-//           }
-
-//           return user;
-//         } catch (error) {
-//           console.error('Authorize error:', error);
-//           throw new Error('Authentication failed, please try again.');
-//         }
-//       },
-//     }),
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-//   pages: {
-//     signIn: '/signin',
-//   },
-//   secret: process.env.NEXTAUTH_SECRET,
-//   session: {
-//     strategy: 'jwt',
-//   },
-//   callbacks: {
-//     async jwt({ token, user }) {
-//       if (user) {
-//         token.id = user.id;
-//         token.email = user.email; 
-//       }
-//       return token;
-//     },
-//     async session({ session, token }) {
-//       if (session.user) {
-//         session.user.id = token.id as string;
-//         session.user.email = token.email as string;
-//       }
-//       return session;
-//     },
-//     async signIn({ user, account }) {
-//       if (account?.provider === 'google') {
-//         const email = user.email as string;
-//       }
-//       return true;
-//     },
-//     async redirect({ url, baseUrl }) {
-//       return url.startsWith(baseUrl) ? url : baseUrl;
-//     },
-//   },
-//   debug: process.env.NODE_ENV !== 'production',
-// };
-
-
-
 import { AuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
@@ -133,20 +51,46 @@ export const authOptions: AuthOptions = {
     strategy: 'jwt',
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, session, trigger }) {
+      console.log("session in jwt callback", session);
       if (user) {
-        token.id = user.id;
-        token.email = user.email;
+        return {
+          ...token,
+          id: user.id,
+          email: user.email,
+          image: user.image,
+          name: user.name,
+        };
       }
+      if (trigger === 'update' && session) {
+        try {
+          const updatedUser = await prisma.user.findUnique({
+            where: { id: token.id as string },
+          });
+
+          if (updatedUser) {
+            token.name = updatedUser.name;
+            token.email = updatedUser.email;
+            token.image = updatedUser.image;
+          }
+        } catch (error) {
+          console.error('Error fetching updated user:', error);
+        }
+      }
+
       return token;
     },
+
     async session({ session, token }) {
       if (session.user) {
         session.user.id = token.id as string;
         session.user.email = token.email as string;
+        session.user.image = token.image as string;
+        session.user.name = token.name as string;
       }
       return session;
     },
+
     async signIn({ user, account }) {
       if (account?.provider === 'google') {
         const googleUser = await prisma.user.findUnique({
@@ -154,7 +98,6 @@ export const authOptions: AuthOptions = {
         });
 
         if (!googleUser) {
-          // If the user does not exist in the database, create a new user
           await prisma.user.create({
             data: {
               email: user.email as string,
@@ -166,6 +109,7 @@ export const authOptions: AuthOptions = {
       }
       return true;
     },
+
     async redirect({ url, baseUrl }) {
       return url.startsWith(baseUrl) ? url : baseUrl;
     },
